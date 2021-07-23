fb9fa0480406ae3fb202f683cdeccabc692f721c
Hoijai Park (Alex) <hoijae0194@naver.com>
2021-07-23 13:17:33 +0900

    feat: create SpringBoot

---
+++ backend/src/test/java/com/kyp/eoneo/EoneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyp.eoneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoneoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
