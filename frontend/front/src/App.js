1272834f548ffc54edc97d9ee6bfa81167c89c4c
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-07-26 16:51:07 +0900

    feat: main category page 제작중

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -9,6 +9,7 @@ import {
 import NavBar from './components/NavBar/NavBar'
 import LoginPage from './components/LoginPage/LoginPage'
 import SignupPage from './components/SignupPage/SignupPage'
+import CategoryPage from './Home/CategoryPage';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       </div>
       <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/main" component={CategoryPage} />
           <Route exact path="/login" component={ LoginPage } />        
           <Route exact path="/signup" component={ SignupPage } />       
 
