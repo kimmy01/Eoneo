4ece6cbdea69cd54eec80df21c87c50d94ca4888
박선아 <bsa8854@naver.com>
2021-07-28 22:38:47 +0900

    chore - @RestControllerAdvice 생성중

---
+++ backend/src/main/java/com/kyp/eoneo/config/advice/ExceptionAdvice.java
@@ -0,0 +1,10 @@
+package com.kyp.eoneo.config.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+}
