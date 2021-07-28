e902622b7c24eacfe72b8a02dbb41e1d158ea4b4
박선아 <bsa8854@naver.com>
2021-07-28 22:33:31 +0900

    refactor - response 형식 수정

--- backend/src/main/java/com/kyp/eoneo/controller/ChatRoomController.java
+++ backend/src/main/java/com/kyp/eoneo/controller/ChatRoomController.java
@@ -1,5 +1,6 @@
 package com.kyp.eoneo.controller;
 
+import com.kyp.eoneo.common.CommonResponse;
 import com.kyp.eoneo.dto.ChatMessageDto;
 import com.kyp.eoneo.dto.ChatRoomDto;
 import com.kyp.eoneo.dto.wrapper.ChatMessageDtoWrapper;
@@ -30,26 +31,35 @@ public class ChatRoomController {
 //채팅방 생성
     @ApiOperation(value = "채팅방생성", notes = "각 사용자의 pk를 통해 새로운 방 생성")
     @PostMapping("/create")
-    public ChatRoomDto createChatRoom(@RequestBody ChatRoomDto chatRoomDto){
+    public CommonResponse<ChatRoomDto> createChatRoom(@RequestBody ChatRoomDto chatRoomDto){
         System.out.println(chatRoomDto.getUser1Id() + " " + chatRoomDto.getUser2Id());
-       ChatRoomDto chatRoom = ChatRoomDto.create(chatRoomDto.getUser1Id(), chatRoomDto.getUser2Id());
-       return chatRoomService.createChatRoom(chatRoom);
+        ChatRoomDto chatRoom = ChatRoomDto.create(chatRoomDto.getUser1Id(), chatRoomDto.getUser2Id());
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setData(chatRoomService.createChatRoom(chatRoom));
+        commonResponse.setSuccess(true);
+       return commonResponse;
     }
 //특정 유저가 가지고 있는 모든 채팅방 리스트
     @ApiOperation(value = "채팅방 리스트", notes = "특정 사용자의 모든 채팅 리스트를 보여준다.")
     @GetMapping("/rooms/{userId}")
-    public ChatRoomDtoWrapper<List<ChatRoomDto>> getChatRoomWithUser(@PathVariable Long userId){
+    public CommonResponse<List<ChatRoomDto>> getChatRoomWithUser(@PathVariable Long userId){
       log.info("한 유저가 가지고 있는 모든 room 다 가져오기" + userId);
       List<ChatRoomDto> list = chatRoomService.getChatRoomList(userId);
-      return new ChatRoomDtoWrapper<List<ChatRoomDto>>(list.size(), list);
+      CommonResponse commonResponse = new CommonResponse();
+      commonResponse.setData(new ChatRoomDtoWrapper<List<ChatRoomDto>>(list.size(), list));
+      commonResponse.setSuccess(true);
+      return commonResponse;
     }
 
 //채팅방 id에 해당하는 chatMessage 가져오기
     @GetMapping("/room/{roomId}")
     @ApiOperation(value = "채팅메시지", notes = "특정 채팅방의 모든 채팅메시지를 보여준다")
-    public ChatMessageDtoWrapper getChatRoomInfo(@PathVariable String roomId){
+    public CommonResponse getChatRoomInfo(@PathVariable String roomId){
         log.info(roomId);
         ChatMessageDtoWrapper chatMessageDtoWrapper = chatRoomService.getChats(roomId);
-        return  chatMessageDtoWrapper;
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setData(chatMessageDtoWrapper);
+        commonResponse.setSuccess(true);
+        return  commonResponse;
     }
 }
