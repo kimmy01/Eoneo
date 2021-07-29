12db63a4b1dd40013169d3e9960227956fc31818
amberpark <amberso1996@gmail.com>
2021-07-29 10:15:00 +0900

    chores: chat button 만듦

--- frontend/front/src/Home/Home.js
+++ frontend/front/src/Home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import {  Image } from 'react-bootstrap'
 // import NavBar from '../components/NavBar/NavBar'
 import MainImage from '../assets/Home/main.jpg' 
 import ProfileCards from './ProfileCards'
 import './Home.css'
+// import Chat from '../Chat/Chat'
 
 
 function Home () {
   return (
     <div>
-   
+      <a href="/chat">
+        Chat
+      </a>
       <div className="image">
         <Image src={MainImage} width='900' height='600' alt="mainimage"/>
       </div>
