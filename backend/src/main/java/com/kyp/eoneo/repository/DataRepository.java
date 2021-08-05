b9ee5838c5324f9c3bf2c94598b10167f546d9be
kimmy01 <nkim40181@hanmail.net>
2021-08-05 11:30:52 +0900

    chore : topic, language, country 관련 레포지토리, 서비스, 컨트롤러 -> data 레포지토리, 서비스, 컨트롤러로 묶음

--- backend/src/main/java/com/kyp/eoneo/repository/DataRepository.java
+++ backend/src/main/java/com/kyp/eoneo/repository/DataRepository.java
@@ -1,6 +1,8 @@
 package com.kyp.eoneo.repository;
 
 import com.kyp.eoneo.entity.Country;
+import com.kyp.eoneo.entity.Language;
+import com.kyp.eoneo.entity.Topic;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,9 +10,17 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-public class CountryRepository {
+public class DataRepository {
     @PersistenceContext
     private EntityManager em;
+
+    public List<Topic> getTopics(){
+        return em.createQuery("select t from Topic t").getResultList();
+    }
+
+    public List<Language> getLanguages(){
+        return em.createQuery("select l from Language l").getResultList();
+    }
 
     public List<Country> getCountries(){
         return em.createQuery("select c from Country c").getResultList();
