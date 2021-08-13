e558ff0f4bd66df998cbe54b88c074405348cb3a
박선아 <bsa8854@naver.com>
2021-08-13 22:02:19 +0900

    build - cors 변경

--- backend/src/main/java/com/kyp/eoneo/config/CorsConfig.java
+++ backend/src/main/java/com/kyp/eoneo/config/CorsConfig.java
@@ -22,7 +22,10 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 //        log.info("come?");
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:80");
+        config.addAllowedOrigin("http://13.124.171.154");
+        config.addAllowedOrigin("http://13.124.171.154:3000");
+        config.addAllowedOrigin("http://172.22.0.1");
+        config.addAllowedOrigin("http://172.22.0.1:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 //        config.addAllowedMethod("PATCH");
