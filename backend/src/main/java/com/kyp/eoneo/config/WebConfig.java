b25c2683445deaa5e3606bb0d7739d6bd7478d4f
박선아 <bsa8854@naver.com>
2021-08-13 11:00:06 +0900

    chore : 포트번호 변경 2

---
+++ backend/src/main/java/com/kyp/eoneo/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.kyp.eoneo.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final String uploadImagesPath;
+
+    public WebConfig(@Value("${custom.path.upload-images") String uploadImagesPath) {
+        this.uploadImagesPath = uploadImagesPath;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/img/**")
+                .addResourceLocations("file:///" + uploadImagesPath + "/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+
+    }
+}
