1d66e0be76e71dd0fc730b5545862108642a7146
박선아 <bsa8854@naver.com>
2021-08-16 11:51:47 +0900

    bugs - image 외부 경로에 저장 관련 에러 수정중3

--- backend/src/main/java/com/kyp/eoneo/service/UserDetailService.java
+++ backend/src/main/java/com/kyp/eoneo/service/UserDetailService.java
@@ -230,7 +230,7 @@ public class UserDetailService {
 
         String absolutePath = new File("").getAbsolutePath()+"/"; //ubuntu에서는 "/"
         System.out.println("absolute Path : " + absolutePath);
-        String path = "/var/eoneo/images";
+        String path = "/tmp/eoneo/images";
 //        System.out.println(rootPath);
 //        String path = "/profileImages/";
 
