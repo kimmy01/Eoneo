6f025ab2f5eff08c1e3c9c1d9aee632cb830ae18
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-08-17 16:54:54 +0900

    fix: chat

--- frontend/front/src/Openvidu/ModalComponent.js
+++ frontend/front/src/Openvidu/ModalComponent.js
@@ -28,7 +28,7 @@ import { faVideo } from "@fortawesome/free-solid-svg-icons";
       <>
        {/* modal button */}
         <Button className="video-button" onClick={() => handleShow(true)}>
-          <FontAwesomeIcon style={{fontSize: 25}} icon={faVideo} />
+          <FontAwesomeIcon style={{fontSize: 25, color: '#a59dff'}} icon={faVideo} />
           {typeof true === 'string' && `below ${true.split('-')[0]}`}
         </Button>
 
