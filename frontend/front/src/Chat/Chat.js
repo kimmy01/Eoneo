43a8a4cf86e41b4861e4a41a569e00458e472e82
박선아 <bsa8854@naver.com>
2021-08-19 06:14:08 +0900

    fix : chat 잔버그

--- frontend/front/src/Chat/Chat.js
+++ frontend/front/src/Chat/Chat.js
@@ -139,9 +139,9 @@ function Chat() {
 	// websocket 연결
 	const connect = (uid) => {
 		client.current = new StompJs.Client({
-			brokerURL: 'wss://api/chatEonoe-websocket',
+			// brokerURL: 'wss://api/chatEonoe-websocket',
 			// webSocketFactory: () => new SockJS("/chatEonoe-websocket"), // proxy를 통한 접속 //internet explore
-			// webSocketFactory: () => new SockJS('/api/chatEonoe-websocket'),
+			webSocketFactory: () => new SockJS('/api/chatEonoe-websocket'),
 			connectHeaders: { Authorization: jwttoken },
 			debug: function (str) {
 				console.log(str);
@@ -286,7 +286,7 @@ function Chat() {
 		event
 	) => {
 		event.stopPropagation();
-
+		disconnect();
 		setRoomId(chatRoomId);
 		getDBdata(chatRoomId);
 		if (user1Id === parseInt(my_id)) {
