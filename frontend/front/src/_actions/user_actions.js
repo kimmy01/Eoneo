8b67e892bf467a1308b29ea4f682ef3c03199ddf
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-08-26 11:32:49 +0900

    feat: login alert

--- frontend/front/src/_actions/user_actions.js
+++ frontend/front/src/_actions/user_actions.js
@@ -1,47 +1,13 @@
 import axios from 'axios'
 import {LOGIN_USER, REGISTER_USER} from './types'
-import swal from '@sweetalert/with-react'
 
-const MoodButton = ({ rating, onClick }) => (
-  <button 
-    data-rating={rating}
-    className="mood-btn" 
-    onClick={() => onClick(rating)}
-  />
-)
-
-const onPick = value => {
-  swal("Thanks for your rating!", `You rated us ${value}/3`, "success")
-}
 
 export function loginUser(dataToSubmit) {
   const request = axios.post('/api/authenticate', dataToSubmit)
       .then(response => response.data)
       .catch(
         
-        swal({
-          text: "How was your experience getting help with this issue?",
-          buttons: {
-            cancel: "Close",
-          },
-          content: (
-            <div>
-              <MoodButton 
-                rating={1} 
-                onClick={onPick}
-              />
-              <MoodButton 
-                rating={2} 
-                onClick={onPick}
-              />
-              <MoodButton 
-                rating={3} 
-                onClick={onPick}
-              />
-            </div>
-          )
-        })
-        
+        alert("confirm email or password")
         )
 
   return {
